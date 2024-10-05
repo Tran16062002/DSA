9f033f4ad664209c84ce9b0c8ca0bcdfd0039668
lien <tranlien.tqp@gmail.com>
2024-10-05 14:57:05 +0300

    lab1

---
+++ Лаб_1/Task1/Task1.py
@@ -0,0 +1,33 @@
+# Insertion Sort in Python
+#function
+from time import perf_counter
+def insertionSort(arr):
+    if n<=1:
+        return
+ 
+    for i in range(1, len(arr)):
+ 
+        key = arr[i]
+   
+        j = i-1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+#result
+t1_start = perf_counter()
+with open('D:\Lab CTDL  - GT\Чан Тхи Лиен_Лаб0 - Copy\Лаб_1\input.txt','r') as infile:
+    n = int(infile.readline())
+    data = str(infile.readline())
+
+data = data.split(' ')
+arr = [int(i) for i in data]
+if n!=len(data) or n<=0 or n>10**3:
+    print('данные не верны')
+else:
+    insertionSort(arr)
+    a = ' '.join(str(i) for i in arr)
+    with open('D:\Lab CTDL  - GT\Чан Тхи Лиен_Лаб0 - Copy\Лаб_1\output.txt', 'w') as outfile:
+        outfile.write(a)
+t1_stop = perf_counter()
+print('Время работы: %s секунд '% (t1_stop - t1_start))
---
+++ Лаб_1/Task4/Task4.py
@@ -0,0 +1,26 @@
+#Linear Search in Python
+def linearSearch(arr,key):
+    list = []
+    for i in range(len(arr)):
+        if arr[i] == key:
+            list.append(i)
+    count = len(list)
+    if count == 0:
+        return str(-1)
+    elif count == 1:
+        return str(list[0])
+    else:
+        return str(count) + '  ' + ",".join(map(str, list))
+        
+with open('D:\Lab CTDL  - GT\Чан Тхи Лиен_Лаб0 - Copy\Лаб_1\input.txt','r') as infile:
+    data = str(infile.readline())
+    key = int(infile.readline())
+
+data = data.split(' ')
+arr = [int(i) for i in data]
+if len(data)<0 or len(data)>10**3 or -10**3>key or key>10**3:
+    print('данные не верны')
+else:
+    
+    with open('D:\Lab CTDL  - GT\Чан Тхи Лиен_Лаб0 - Copy\Лаб_1\output.txt', 'w') as outfile:
+        outfile.write(linearSearch(arr,key))
